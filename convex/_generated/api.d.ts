9107b70884d6027550aaaf50626449e7dc8bc4c9
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-10-10 11:45:29 +0700

    feat: update deps with convex update

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -253,31 +253,32 @@ export declare const components: {
           };
         }
       >;
-      startStep: FunctionReference<
+      startSteps: FunctionReference<
         "mutation",
         "internal",
         {
           generationNumber: number;
-          name: string;
-          retry?:
-            | boolean
-            | { base: number; initialBackoffMs: number; maxAttempts: number };
-          schedulerOptions?: { runAt?: number } | { runAfter?: number };
-          step: {
-            args: any;
-            argsSize: number;
-            completedAt?: number;
-            functionType: "query" | "mutation" | "action";
-            handle: string;
-            inProgress: boolean;
-            name: string;
-            runResult?:
-              | { kind: "success"; returnValue: any }
-              | { error: string; kind: "failed" }
-              | { kind: "canceled" };
-            startedAt: number;
-            workId?: string;
-          };
+          steps: Array<{
+            retry?:
+              | boolean
+              | { base: number; initialBackoffMs: number; maxAttempts: number };
+            schedulerOptions?: { runAt?: number } | { runAfter?: number };
+            step: {
+              args: any;
+              argsSize: number;
+              completedAt?: number;
+              functionType: "query" | "mutation" | "action";
+              handle: string;
+              inProgress: boolean;
+              name: string;
+              runResult?:
+                | { kind: "success"; returnValue: any }
+                | { error: string; kind: "failed" }
+                | { kind: "canceled" };
+              startedAt: number;
+              workId?: string;
+            };
+          }>;
           workflowId: string;
           workpoolOptions?: {
             defaultRetryBehavior?: {
@@ -290,7 +291,7 @@ export declare const components: {
             retryActionsByDefault?: boolean;
           };
         },
-        {
+        Array<{
           _creationTime: number;
           _id: string;
           step: {
@@ -310,7 +311,7 @@ export declare const components: {
           };
           stepNumber: number;
           workflowId: string;
-        }
+        }>
       >;
     };
     workflow: {
