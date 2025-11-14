898669424b3c779394f936f180f8c75ab05b6686
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-11-14 09:12:11 +0700

    feat: increase max attempts because the AI keep failing but if we retry tomorrow it works

--- convex/index.ts
+++ convex/index.ts
@@ -4,6 +4,12 @@ import { components } from "./_generated/api";
 export const workflow = new WorkflowManager(components.workflow, {
   workpoolOptions: {
     retryActionsByDefault: true,
+    /** @docs https://www.convex.dev/components/workpool#completion-handling */
+    defaultRetryBehavior: {
+      maxAttempts: 30,
+      initialBackoffMs: 1000,
+      base: 2,
+    },
     maxParallelism: 15, // pay-as-you-go plan max is 16, so we set 15 for now. can up this number when we upgrade our plan.
   },
 });
