8f2ed6c8267309d1689d02dd1059c3deff342696
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-12-18 21:25:54 +0700

    refactor: simplify

--- convex/domains/analysisWorkflows/queries.ts
+++ convex/domains/analysisWorkflows/queries.ts
@@ -1,7 +1,6 @@
 import type { PaginationResult } from "convex/server";
 import { paginationOptsValidator } from "convex/server";
 import { v } from "convex/values";
-import type { Doc } from "../../_generated/dataModel";
 import { query } from "../../_generated/server";
 import { authComponent } from "../../auth";
 
@@ -19,9 +18,7 @@ export const getPaginatedAnalysisWorkflowHeaders = query({
         page: [],
         isDone: true,
         continueCursor: "",
-      } satisfies PaginationResult<
-        Doc<"analysisWorkflowHeaders"> & { createdByUser: Doc<"users"> | null }
-      >;
+      } satisfies PaginationResult<unknown>;
     }
     const userId = authUser._id as string;
     console.log("userId", userId);
