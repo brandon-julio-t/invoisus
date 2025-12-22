c6dd61de7fa25b77ac1a8c796a87724c43ed3242
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-12-22 20:18:31 +0700

    feat: proper indentation for list 2nd..nth lines

--- components/tip-tap-editor.tsx
+++ components/tip-tap-editor.tsx
@@ -222,9 +222,9 @@ export function TipTapEditor({
           "[&_p]:py-0.5",
 
           // Ordered List styles
-          "[&_ul]:list-disc [&_ul]:list-inside [&_ul_p]:inline",
+          "[&_ul]:list-disc [&_ul]:list-outside [&_ul]:ml-4.5 [&_ul_p]:inline",
           // Bullet List styles
-          "[&_ol]:list-decimal [&_ol]:list-inside [&_ol_p]:inline",
+          "[&_ol]:list-decimal [&_ol]:list-outside [&_ol]:ml-4.5 [&_ol_p]:inline",
           className,
         )}
         {...props}
